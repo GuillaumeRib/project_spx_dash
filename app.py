1f9879a777972929ccd55a45ef8474529107b848
Guillaume Ribiere <gribiere@gmail.com>
2022-11-27 21:22:14 +0100

    change name app to avoid conflict to spx_app

--- app.py
+++ app.py
@@ -30,11 +30,11 @@ df = get_data.get_returns_period(returns_df,df)
 # INIT APP
 ####################################
 dbc_css = ("https://cdn.jsdelivr.net/gh/AnnMarieW/dash-bootstrap-templates@V1.0.2/dbc.min.css")
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.LUX, dbc_css],
+spx_app = dash.Dash(__name__, external_stylesheets=[dbc.themes.LUX, dbc_css],
                 meta_tags=[{'name':'viewport',
                             'content':'width=device-width,initial-scale=1.0'}]
                 )
-server=app.server
+server=spx_app.server
 
 
 
@@ -70,7 +70,7 @@ xxl=6
 
 
 
-app.layout = dbc.Container([
+spx_app.layout = dbc.Container([
     dbc.Row([
         dbc.Col(title,width=12,class_name=('mt-4'))
     ]),
@@ -113,5 +113,5 @@ app.layout = dbc.Container([
 # RUN the app
 ####################################
 if __name__ == '__main__':
-    server=app.server
-    app.run_server(debug=True)
+    server=spx_app.server
+    spx_app.run_server(debug=True)
